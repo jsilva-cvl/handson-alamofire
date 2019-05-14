67b16d233977b7a5ad131f21a6e586d931059e59
Jose Silva <jsilva@codavel.com>
2019-05-14 18:22:05 +0100

    Initial commit

---
+++ howto-alamofire/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  howto-alamofire
+//
+//  Created by José Silva on 14/05/2019.
+//  Copyright © 2019 Codavel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func §(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+
+
+}
+
